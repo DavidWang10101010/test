089a82b11273f56fc1fa0a4b6fd4b7be03648ab6
DavidWang10101010 <2978624033@qq.com>
2020-11-22 00:04:27 +0800

    项目人员01号：第一次测试提交信息01

--- src/main/java/com/test/demo/TestApplication.java
+++ src/main/java/com/test/demo/TestApplication.java
@@ -3,5 +3,6 @@ package com.test.demo;
 public class TestApplication {
     public static void main(String[] args) {
         System.out.println("Hello: first time test 02");
+        System.out.println("TestSource commit 01");
     }
 }
