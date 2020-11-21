7ef4353abdedffa30694bbaf83b1c15f996ce7fa
DavidWang10101010 <2978624033@qq.com>
2020-11-21 23:40:58 +0800

    第一次测试文件02

--- src/main/java/com/test/demo/TestApplication.java
+++ src/main/java/com/test/demo/TestApplication.java
@@ -2,6 +2,6 @@ package com.test.demo;
 
 public class TestApplication {
     public static void main(String[] args) {
-        System.out.println("Hello: first time test......");
+        System.out.println("Hello: first time test 02");
     }
 }
