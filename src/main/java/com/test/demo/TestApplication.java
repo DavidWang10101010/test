57d90913428ae5e5238287d58cb5214f51212772
DavidWang10101010 <2978624033@qq.com>
2020-11-23 00:18:18 +0800

    第一次测试文件：两台电脑互传01

--- src/main/java/com/test/demo/TestApplication.java
+++ src/main/java/com/test/demo/TestApplication.java
@@ -5,5 +5,6 @@ public class TestApplication {
         System.out.println("Hello: first time test 02");
         System.out.println("TestSource commit 01");
         System.out.println("1234561110004");
+        System.out.println("这是我的提交！！");
     }
 }
