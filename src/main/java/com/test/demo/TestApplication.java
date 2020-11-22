d851c7571207107be8c1b5546a0144fab9a884e6
DavidWang10101010 <2978624033@qq.com>
2020-11-23 00:07:30 +0800

    振飞的第一次提交：初始化03

--- src/main/java/com/test/demo/TestApplication.java
+++ src/main/java/com/test/demo/TestApplication.java
@@ -4,6 +4,6 @@ public class TestApplication {
     public static void main(String[] args) {
         System.out.println("Hello: first time test 02");
         System.out.println("TestSource commit 01");
-        System.out.println("123456111");
+        System.out.println("12345611100");
     }
 }
